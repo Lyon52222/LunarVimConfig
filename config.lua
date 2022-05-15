1d758b57e7ad0f08715ba0721d94a4df01359657
lyon <1765813715@qq.com>
2022-05-16 00:20:19 +0800

    add session-manager

--- config.lua
+++ config.lua
@@ -66,10 +66,8 @@ lvim.builtin.which_key.mappings["R"] = {
     name = "+SnipRun",
     r = { "<cmd>SnipRun<cr>", "RunCode" },
     c = { "<cmd>SnipClose<cr>", "Clean" },
-    -- R = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
-    -- l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
-    -- Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
 }
+
 
 lvim.builtin.which_key.mappings["D"] = {
     name = "+Todo",
@@ -360,17 +358,25 @@ lvim.plugins = {
             }
         end
     },
-    -- {
-    --     "folke/persistence.nvim",
-    --     event = "BufReadPre", -- this will only start session saving when an actual file was opened
-    --     module = "persistence",
-    --     config = function()
-    --         require("persistence").setup {
-    --             dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
-    --             options = { "buffers", "curdir", "tabpages", "winsize" },
-    --         }
-    --     end,
-    -- },
+    { --session自动保存加载
+        "Shatur/neovim-session-manager",
+        config = function()
+            local Path = require('plenary.path')
+            require('session_manager').setup({
+                sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
+                path_replacer = '__', -- The character to which the path separator will be replaced for session files.
+                colon_replacer = '++', -- The character to which the colon symbol will be replaced for session files.
+                autoload_mode = require('session_manager.config').AutoloadMode.LastSession, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+                autosave_last_session = true, -- Automatically save last session on exit and on session switch.
+                autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
+                autosave_ignore_filetypes = { -- All buffers of these file types will be closed before the session is saved.
+                    'gitcommit',
+                },
+                autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
+                max_path_length = 80, -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
+            })
+        end
+    },
     { --快速添加函数注释，类注释
         "kkoomen/vim-doge" },
     { --自动补全cmd
